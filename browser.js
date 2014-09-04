b9526ffca099d64c5129a68afedc8b0f7e895568
Jorik Tangelder <j.tangelder@gmail.com>
2014-09-04 10:29:45 +0200

    restored the orginal code, and re-added the canMutationObserver

--- browser.js
+++ browser.js
@@ -1,87 +1,82 @@
 // shim for using process in browser
 
+var process = module.exports = {};
+
+process.nextTick = (function () {
+    var canSetImmediate = typeof window !== 'undefined' && window.setImmediate;
+    var canMutationObserver = typeof window !== 'undefined' && typeof document !== 'undefined' && window.MutationObserver;
+    var canPost = typeof window !== 'undefined' && window.postMessage && window.addEventListener;
+
+    if (canSetImmediate) {
+        return function (f) { return window.setImmediate(f) };
+    }
+
+    var queue = [];
+
+    if (canMutationObserver) {
+        var hiddenDiv = document.createElement("div");
+        var observer = new MutationObserver(function () {
+            var queueList = queue.slice();
+            queue.length = 0;
+            queueList.forEach(function (fn) {
+                fn();
+            });
+        });
+
+        observer.observe(hiddenDiv, { attributes: true });
+
+        return function nextTick(fn) {
+            if (!queue.length) {
+                hiddenDiv.setAttribute('yes', 'no');
+            }
+            queue.push(fn);
+        };
+    }
+
+    if (canPost) {
+        window.addEventListener('message', function (ev) {
+            var source = ev.source;
+            if ((source === window || source === null) && ev.data === 'process-tick') {
+                ev.stopPropagation();
+                if (queue.length > 0) {
+                    var fn = queue.shift();
+                    fn();
+                }
+            }
+        }, true);
+
+        return function nextTick(fn) {
+            queue.push(fn);
+            window.postMessage('process-tick', '*');
+        };
+    }
+
+    return function nextTick(fn) {
+        setTimeout(fn, 0);
+    };
+})();
+
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+
 function noop() {}
 
-module.exports = {
-    nextTick : (function () {
-        var win = typeof window !== 'undefined' && window,
-            doc = win && win.document,
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
 
-            setImmediate = win && win.setImmediate,
-            MutationObserver = doc && win.MutationObserver,
-            postMessage = win.addEventListener && win.postMessage,
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+}
 
-            queue = [];
-
-        if (setImmediate) {
-            return function (fn) {
-                return setImmediate(fn);
-            };
-        }
-
-        if (MutationObserver) {
-            var hiddenDiv = doc.createElement("div");
-            var observer = new MutationObserver(function () {
-                var fnList = queue.slice();
-                queue.length = 0;
-                fnList.forEach(function (fn) {
-                    fn();
-                });
-            });
-
-            observer.observe(hiddenDiv, { attributes: true });
-
-            return function (fn) {
-                if (!queue.length) {
-                    hiddenDiv.setAttribute('yes', 'no');
-                }
-                queue.push(fn);
-            };
-        }
-
-        if (postMessage) {
-            var msgData = 'process-tick';
-            win.addEventListener('message', function (ev) {
-                var source = ev.source;
-                if ((source === win || source === null) && ev.data === msgData) {
-                    ev.stopPropagation();
-                    if (queue.length > 0) {
-                        queue.shift()();
-                    }
-                }
-            }, true);
-
-            return function (fn) {
-                queue.push(fn);
-                postMessage(msgData, '*');
-            };
-        }
-
-        return function (fn) {
-            setTimeout(fn, 0);
-        };
-    })(),
-
-    title: 'browser',
-    browser: true,
-    env: {},
-    argv: [],
-
-    on: noop,
-    addListener: noop,
-    once: noop,
-    off: noop,
-    removeListener: noop,
-    removeAllListeners: noop,
-    emit: noop,
-
-    binding: function (name) {
-        throw new Error('process.binding is not supported');
-    },
-
-    // TODO(shtylman)
-    cwd: function () { return '/' },
-    chdir: function (dir) {
-        throw new Error('process.chdir is not supported');
-    }
+// TODO(shtylman)
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
 };
